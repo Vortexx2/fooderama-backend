8cd63b2137114a3fc889aa428c3b76bac23a3a11
Vortexx <janmayshuklay@gmail.com>
2022-06-27 23:29:33 +0530

    Added 2 error handlers

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -16,9 +16,28 @@ function logCompleteInfo(req: Request, res: Response, next: NextFunction) {
   next();
 }
 
+// TODO: customise TS to understand custom `err` type
+/**
+ * Middleware to log the error to the correct transport. 
+ * @param err Error of custom type (eventually will be implemented)
+ * @param req Request
+ * @param res Response
+ * @param next Next
+ */
 const logError: ErrorRequestHandler = (err, req, res, next) => {
   logger.error(err.message);
   next();
 };
+
+/**
+ * Final middleware to send json of error to user. Will contain stacktrace as well.
+ * @param err Error of custom type (eventually will be implemented)
+ * @param req Request
+ * @param res Response
+ * @param next Next
+ */
+const errorResponder: ErrorRequestHandler = (err, req, res, next) => {
+  res.status(err.statusCode).json(err);
+}
 
 export { logReqInfo, logCompleteInfo, logError };
