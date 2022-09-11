6551d8678d69ded8561d6487ef782f57615c690a
Vortexx2 <janmayshuklay@gmail.com>
2022-09-12 00:16:18 +0530

    Reconfigured user validation schema to match frontend

--- src/constants/users.ts
+++ src/constants/users.ts
@@ -2,6 +2,7 @@
 export const userValidationConfig = {
   EMAIL_REGEX:
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+  PASSWORD_REGEX: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/,
   MAX_EMAIL_LEN: 256,
   MIN_PASSWORD_LEN: 6,
   MAX_PASSWORD_LEN: 30,
--- src/utils/zodSchemas/userSchema.ts
+++ src/utils/zodSchemas/userSchema.ts
@@ -6,26 +6,28 @@ import validator from 'validator'
 // Imports above
 
 export const zUser = z.object({
-  email: z.string().trim().max(config.MAX_EMAIL_LEN).regex(config.EMAIL_REGEX),
-  password: z
+  email: z
     .string()
     .trim()
-    .min(config.MIN_PASSWORD_LEN)
-    .max(config.MAX_PASSWORD_LEN)
-    .refine(
-      pass =>
-        validator.isStrongPassword(pass, {
-          minLength: config.MIN_PASSWORD_LEN,
-          minLowercase: 0,
-          minUppercase: 0,
-          minNumbers: 1,
-          minSymbols: 0,
-          returnScore: false,
-        }),
-      {
-        message:
-          'Password must be minimum 6 characters in length with minimum 1 lowercase and 1 numerical character',
-      }
+    .max(config.MAX_EMAIL_LEN, {
+      message: 'Email must have at most 256 characters',
+    })
+    .regex(config.EMAIL_REGEX, {
+      message: 'Email is not valid',
+    }),
+  password: z
+    .string()
+    .min(
+      config.MIN_PASSWORD_LEN,
+      `Password must be at least ${config.MIN_PASSWORD_LEN} characters in length`
+    )
+    .max(
+      config.MAX_PASSWORD_LEN,
+      `Password must be less than ${config.MAX_PASSWORD_LEN} characters in length`
+    )
+    .regex(
+      config.PASSWORD_REGEX,
+      'Password must have at least 1 character and 1 number'
     ),
 })
 
